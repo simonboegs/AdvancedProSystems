c3ccbb67700a23d92c8f22e1224e9d7182ae3fc7
Simon Boeger <simon@boegerweb.com>
2021-09-10 16:49:17 -0700

    added mobile support for contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -125,9 +125,101 @@ export function ContactForm(props) {
     </div>
   );
 
+  const mobile = (
+    <div
+      className="container"
+      style={{
+        border: "3px solid gray",
+        maxWidth: "800px",
+        borderRadius: "10px",
+        background: "rgb(255,255,255,.5)",
+        padding: "20px",
+      }}
+    >
+      <div className={formContainer}>
+        <h1 className="text-center">Get More Info</h1>
+        <div id="options" className="row justify-content-center py-2 gy-2">
+          <div className="col-12">
+            <h5>Which best describes you?</h5>
+          </div>
+          <div className="col-12">
+            <input id="option1" type="radio" name="yo" value="yo" />
+            <label className={optionLabel} for="option1">
+              I have equipment that needs to get serviced.
+            </label>
+          </div>
+          <div className="col-12">
+            <input id="option2" type="radio" name="yo" value="yo" />
+            <label className={optionLabel} for="option2">
+              I need to plan services for equipment I do not yet have.
+            </label>
+          </div>
+          <div className="col-12">
+            <input id="option3" type="radio" name="yo" value="yo" />
+            <label className={optionLabel} for="option3">
+              I need equipment!
+            </label>
+          </div>
+          <div className="col-12">
+            <input id="option3" type="radio" name="yo" value="yo" />
+            <input
+              className={optionTextInput}
+              placeholder="Please tell us about your needs!"
+            ></input>
+          </div>
+        </div>
+        <div className="row justify-content-center py-2 gy-2">
+          <div className="col-6">
+            <label for="nameInput fw-bold">Name</label>
+            <input className={textInput} id="nameInput"></input>
+          </div>
+          <div className="col-6">
+            <label for="nameInput fw-bold">Company</label>
+            <input className={textInput} id="nameInput"></input>
+          </div>
+          <div class="w-100"></div>
+          <div className="col-6">
+            <label for="nameInput fw-bold">Email</label>
+            <input className={textInput} id="nameInput"></input>
+          </div>
+          <div className="col-6">
+            <label for="nameInput fw-bold">Best Phone</label>
+            <input className={textInput} id="nameInput"></input>
+          </div>
+          <div class="w-100"></div>
+        </div>
+        <div className="row justify-content-center py-2">
+          <div className="col-6">
+            <CountryDropdown
+              className={dropdown}
+              value={formData.country}
+              onChange={(val) => setCountry(val)}
+            />
+          </div>
+          <div className="col-6">
+            <RegionDropdown
+              country={formData.country}
+              className={dropdown}
+              value={formData.region}
+              onChange={(val) => setRegion(val)}
+              disableWhenEmpty
+            />
+          </div>
+        </div>
+        <div className="row justify-content-center py-3">
+          <div className="col-12">
+            <button className="btn btn-primary" style={{ width: "100%" }}>
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
   return (
     <section style={{ background: props.bgColor, padding: "20px 0 20px 0" }}>
-      <form>{width > breakpoint ? desktop : desktop}</form>
+      <form>{width > breakpoint ? desktop : mobile}</form>
     </section>
   );
 }
