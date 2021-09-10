c07b6d07cca8bce3b9e8084b1062998d1e4dc7ce
Simon Boeger <simon@boegerweb.com>
2021-09-10 10:04:07 -0700

    added country/region selection and submit button to contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,12 +1,37 @@
 import * as React from "react";
 import { useViewport } from "./Layout";
-import { textInput, formContainer, optionLabel, optionTextInput } from "./ContactForm.module.css";
+import {
+  textInput,
+  formContainer,
+  optionLabel,
+  optionTextInput,
+  dropdown,
+} from "./ContactForm.module.css";
+import { CountryDropdown, RegionDropdown } from "react-country-region-selector";
+import { StaticQuery } from "gatsby";
 const bootstrap =
   typeof window !== `undefined` ? require("bootstrap/dist/css/bootstrap.min.css") : null;
 
 export function ContactForm(props) {
   const { width } = useViewport();
   const breakpoint = 768;
+
+  const [formData, setFormData] = React.useState({
+    country: "United States",
+    region: "",
+  });
+
+  const setCountry = (val) => {
+    setFormData({
+      country: val,
+    });
+  };
+
+  const setRegion = (val) => {
+    setFormData({
+      region: val,
+    });
+  };
 
   const desktop = (
     <div
@@ -20,54 +45,80 @@ export function ContactForm(props) {
       }}
     >
       <div className={formContainer}>
-        <h1 className="text-center">Contact</h1>
-        <div className="row justify-content-center py-3 gy-2">
-          <div className="col-4">
-            <label for="nameInput fw-bold">Name</label>
-            <input className={textInput} id="nameInput"></input>
+        <h1 className="text-center">Get More Info</h1>
+        <div id="options" className="row justify-content-center py-2 gy-2">
+          <div className="col-10">
+            <h5>Which best describes you?</h5>
           </div>
-          <div className="col-4">
-            <label for="nameInput fw-bold">Company</label>
-            <input className={textInput} id="nameInput"></input>
-          </div>
-          <div class="w-100"></div>
-          <div className="col-4">
-            <label for="nameInput fw-bold">Email</label>
-            <input className={textInput} id="nameInput"></input>
-          </div>
-          <div className="col-4">
-            <label for="nameInput fw-bold">Best Phone</label>
-            <input className={textInput} id="nameInput"></input>
-          </div>
-        </div>
-        <div id="options" className="row justify-content-center py-3 gy-2">
-          <div className="col-8">
-            <p>Which best describes you?</p>
-          </div>
-          <div className="col-7">
+          <div className="col-9">
             <input id="option1" type="radio" name="yo" value="yo" />
             <label className={optionLabel} for="option1">
               I have equipment that needs to get serviced.
             </label>
           </div>
-          <div className="col-7">
+          <div className="col-9">
             <input id="option2" type="radio" name="yo" value="yo" />
             <label className={optionLabel} for="option2">
               I need to plan services for equipment I do not yet have.
             </label>
           </div>
-          <div className="col-7">
+          <div className="col-9">
             <input id="option3" type="radio" name="yo" value="yo" />
             <label className={optionLabel} for="option3">
               I need equipment!
             </label>
           </div>
-          <div className="col-7">
+          <div className="col-9">
             <input id="option3" type="radio" name="yo" value="yo" />
             <input
               className={optionTextInput}
               placeholder="Please tell us about your needs!"
             ></input>
+          </div>
+        </div>
+        <div className="row justify-content-center py-2 gy-2">
+          <div className="col-5">
+            <label for="nameInput fw-bold">Name</label>
+            <input className={textInput} id="nameInput"></input>
+          </div>
+          <div className="col-5">
+            <label for="nameInput fw-bold">Company</label>
+            <input className={textInput} id="nameInput"></input>
+          </div>
+          <div class="w-100"></div>
+          <div className="col-5">
+            <label for="nameInput fw-bold">Email</label>
+            <input className={textInput} id="nameInput"></input>
+          </div>
+          <div className="col-5">
+            <label for="nameInput fw-bold">Best Phone</label>
+            <input className={textInput} id="nameInput"></input>
+          </div>
+          <div class="w-100"></div>
+        </div>
+        <div className="row justify-content-center py-2">
+          <div className="col-5">
+            <CountryDropdown
+              className={dropdown}
+              value={formData.country}
+              onChange={(val) => setCountry(val)}
+            />
+          </div>
+          <div className="col-5">
+            <RegionDropdown
+              country={formData.country}
+              className={dropdown}
+              value={formData.region}
+              onChange={(val) => setRegion(val)}
+              disableWhenEmpty
+            />
+          </div>
+        </div>
+        <div className="row justify-content-center py-3">
+          <div className="col-10">
+            <button className="btn btn-primary" style={{ width: "100%" }}>
+              Submit
+            </button>
           </div>
         </div>
       </div>
