db4f6b71c87e49c5c24a5648c94a0dedfccd3806
Simon Boeger <simon@boegerweb.com>
2021-09-10 16:15:27 -0700

    added offcanvas

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,29 +3,42 @@ import { Link } from "gatsby";
 import logoImg from "../images/logo.png";
 import justify from "../images/justify.svg";
 import { useViewport } from "./Layout";
+import OffCanvas from "react-aria-offcanvas";
 const bootstrap =
   typeof window !== `undefined` ? require("bootstrap/dist/css/bootstrap.min.css") : null;
+
+function offcanvas() {
+  return (
+    <nav id="menu">
+      <ul>
+        <li>
+          <a href="#m-i-1">Menu Item 1</a>
+        </li>
+        <li>
+          <a href="#m-i-2">Menu Item 2</a>
+        </li>
+        <li>
+          <a href="#m-i-3">Menu Item 3</a>
+        </li>
+        <li>
+          <a href="#m-i-4">Menu Item 4</a>
+        </li>
+        <li>
+          <a href="#m-i-5">Menu Item 5</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
 
 export function NavBar(props) {
   const { width } = useViewport();
   const breakpoint = 768;
 
-  const offcanvas = (
-    <div className="offcanvas offcanvas-start" id="offcanvasNav">
-      <div className="offcanvas-header">
-        <h5 class="offcanvas-title">Offcanvas</h5>
-        <button
-          type="button"
-          class="btn-close text-reset"
-          data-bs-dismiss="offcanvas"
-          aria-label="Close"
-        ></button>
-        <div class="offcanvas-body">
-          <div>whatup</div>
-        </div>
-      </div>
-    </div>
-  );
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const open = () => setIsOpen(true);
+  const close = () => setIsOpen(false);
 
   const desktop = (
     <div className="container">
@@ -70,48 +83,68 @@ export function NavBar(props) {
         </Link>
         <ul className="nav nav-pills">
           <li className="nav-item">
-            <button className="navbar-toggler">
-              <a
-                href={offcanvas}
-                data-bs-toggle="offcanvas"
-                role="button"
-                aria-controls="offcanvasNav"
-              >
-                <img height="40px" src={justify} />
-              </a>
+            <button onClick={open} className="navbar-toggler">
+              <img height="40px" src={justify} />
             </button>
+            <OffCanvas
+              style={{
+                content: {
+                  background: "white",
+                  borderRadius: "5px",
+                  marginTop: "85px",
+                },
+              }}
+              isOpen={isOpen}
+              onClose={close}
+              closeOnEsc
+              position="right"
+              height="160px"
+            >
+              <div className="" style={{ height: "100%" }}>
+                <ul className="nav flex-column nav-pills">
+                  <li className="nav-item active">
+                    <Link to="/" className="nav-link" activeClassName="active" aria-current="page">
+                      Home
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link
+                      to="/about"
+                      className="nav-link"
+                      activeClassName="active"
+                      aria-current="page"
+                    >
+                      About
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link
+                      to="/services"
+                      className="nav-link"
+                      activeClassName="active"
+                      aria-current="page"
+                    >
+                      Services
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link
+                      to="/contact"
+                      className="nav-link"
+                      activeClassName="active"
+                      aria-current="page"
+                    >
+                      <strong>Contact</strong>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </OffCanvas>
           </li>
         </ul>
       </div>
     </div>
   );
-
-  // const mobile = (
-  //   <div className="container">
-  //     <div className="row justify-content-center align-items-center py-1">
-  //       <div className="col">
-  //         <Link
-  //           to="/"
-  //           className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-decoration-none"
-  //         >
-  //           <img class="bi me-2" height="100px" src={logoImg} />
-  //         </Link>
-  //       </div>
-  //       <div className="col text-end">
-  //         <button className="navbar-toggler">
-  //           <a
-  //             href={offcanvas}
-  //             data-bs-toggle="offcanvas"
-  //             role="button"
-  //             aria-controls="offcanvasNav"
-  //           >
-  //             <img height="40px" src={justify} />
-  //           </a>
-  //         </button>
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
 
   return (
     <section style={{ background: props.bgColor }}>{width < breakpoint ? mobile : desktop}</section>
