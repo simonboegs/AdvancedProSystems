dca669f1d7864094e6c1652f4371acf6379dcad2
Simon Boeger <simonboeger@Simons-MacBook-Pro-2.local>
2022-03-29 08:26:01 -0700

    hooked up form first try

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -20,19 +20,68 @@ export function ContactForm(props) {
   const [formData, setFormData] = React.useState({
     country: "United States",
     region: "",
+    name: "",
+    email: "",
+    phone: "",
+    radioSelection: "option1"
   });
 
+  const handleChange = e => {
+    setFormData((prevState) => {
+      return {
+        ...prevState,
+        [e.target.name]: e.target.value
+      }
+    });
+    console.log(formData);
+  }
+
+  const setRadioSelection = val => {
+    setFormData((prevState) => {
+      return {
+        ...prevState,
+        radioSelection: val
+      }
+    });
+    console.log(formData);
+  }
+
   const setCountry = (val) => {
-    setFormData({
-      country: val,
+    setFormData((prevState) => {
+      return {
+        ...prevState,
+        country: val
+      }
     });
   };
 
   const setRegion = (val) => {
-    setFormData({
-      region: val,
+    setFormData((prevState) => {
+      return {
+        ...prevState,
+        region: val
+      }
     });
   };
+
+  const handleSubmit = e => {
+    console.log("form submit");
+    console.log(formData);
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...formData })
+    })
+    .then(() => alert("Success!"))
+    .catch(error => alert(error));
+    e.preventDefault()
+  }
+
+  const encode = (data) => {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
+  }
 
   const desktop = (
     <div
@@ -45,37 +94,38 @@ export function ContactForm(props) {
         padding: "20px",
       }}
     >
+      <form onSubmit={handleSubmit}>
       <div className={formContainer}>
         <h1 className="text-center">Get More Info</h1>
-        <div id="options" className="row justify-content-center py-2 gy-2">
+        <div id="options" onChange={(e) => setRadioSelection(e.target.value)} className="row justify-content-center py-2 gy-2" name="radioSelection">
           <div className="col-10">
             <h5>Which best describes you?</h5>
           </div>
           <div className="col-9">
-            <input id="option1" type="radio" name="yo" value="yo" />
+            <input id="option1" type="radio" value="option1" checked={formData.radioSelection == "option1"}/>
             <label className={optionLabel} for="option1">
               I have equipment that needs to get serviced.
             </label>
           </div>
           <div className="col-9">
-            <input id="option2" type="radio" name="yo" value="yo" />
+            <input id="option2" type="radio" value="option2" checked={formData.radioSelection == "option2"}/>
             <label className={optionLabel} for="option2">
               I need to plan services for equipment I do not yet have.
             </label>
           </div>
           <div className="col-9 align-items-center">
-            <input id="option3" type="radio" name="yo" value="yo" />
+            <input id="option3" type="radio" value="option3" checked={formData.radioSelection == "option3"}/>
             <label className={optionLabel} for="option3">
               I need equipment!
             </label>
           </div>
           <div className="col-9">
             <input
-              id="option3"
+              id="option4"
               type="radio"
-              name="yo"
-              value="yo"
+              value="option4"
               style={{ verticalAlign: "top" }}
+              checked={formData.radioSelection == "option4"}
             />
             <TextareaAutosize className={optionTextInput} minRows="1" />
           </div>
@@ -83,20 +133,20 @@ export function ContactForm(props) {
         <div className="row justify-content-center py-2 gy-2">
           <div className="col-5">
             <label for="nameInput fw-bold">Name</label>
-            <input className={textInput} id="nameInput"></input>
+            <input className={textInput} id="nameInput" name="name" onChange={handleChange}></input>
           </div>
           <div className="col-5">
             <label for="nameInput fw-bold">Company</label>
-            <input className={textInput} id="nameInput"></input>
+            <input className={textInput} id="nameInput" name="company" onChange={handleChange}></input>
           </div>
           <div class="w-100"></div>
           <div className="col-5">
             <label for="nameInput fw-bold">Email</label>
-            <input className={textInput} id="nameInput"></input>
+            <input className={textInput} id="nameInput" name="email" onChange={handleChange}></input>
           </div>
           <div className="col-5">
             <label for="nameInput fw-bold">Best Phone</label>
-            <input className={textInput} id="nameInput"></input>
+            <input className={textInput} id="nameInput" name="phone" onChange={handleChange}></input>
           </div>
           <div class="w-100"></div>
         </div>
@@ -120,12 +170,13 @@ export function ContactForm(props) {
         </div>
         <div className="row justify-content-center py-3">
           <div className="col-10">
-            <button className="btn btn-primary" style={{ width: "100%" }}>
+            <button className="btn btn-primary" style={{ width: "100%" }} type="submit">
               Submit
             </button>
           </div>
         </div>
       </div>
+      </form>
     </div>
   );
 
@@ -212,7 +263,7 @@ export function ContactForm(props) {
         </div>
         <div className="row justify-content-center py-3">
           <div className="col-12">
-            <button className="btn btn-primary" style={{ width: "100%" }}>
+            <button className="btn btn-primary" style={{ width: "100%" }} type="submit">
               Submit
             </button>
           </div>
@@ -223,7 +274,7 @@ export function ContactForm(props) {
 
   return (
     <section style={{ background: props.bgColor, padding: "20px 0 20px 0" }}>
-      <form>{width > breakpoint ? desktop : mobile}</form>
+      {width > breakpoint ? desktop : mobile}
     </section>
   );
 }
